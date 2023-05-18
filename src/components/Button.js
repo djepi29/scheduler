a4c6ed9e6793a6f14e628c1b29e154236684acdd
djepi29 <j3anpi3rr3g3org3s@email.com>
2023-05-18 18:29:03 -0400

    button component setup

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,36 @@
 import React from "react";
+import classNames from "classnames";
 
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+   //////////////////////////
+//   let buttonClass = "button";
+
+//   if (props.confirm) {
+//     buttonClass += " button--confirm";
+//   } else if (props.danger) {
+//     buttonClass += " button--danger";
+//   }
+
+////conversion
+
+const buttonClass = classNames("button", {
+   "button--confirm": props.confirm,
+   "button--danger": props.danger
+ });
+
+//////////////////////////////
+
+
+
+  return (
+    <button
+      onClick={props.onClick}
+      disabled={props.disabled}
+      className={buttonClass}
+    >
+      {props.children}
+    </button>
+  );
 }
