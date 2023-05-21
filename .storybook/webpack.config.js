57511c1bf9889f557354ae68a92865711d6bc906
djepi29 <j3anpi3rr3g3org3s@email.com>
2023-05-21 09:16:16 -0400

    update sass

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,3 +1,15 @@
+// const path = require("path");
+
+// module.exports = async ({ config, mode }) => {
+//   return {
+//     ...config,
+//     resolve: {
+//       ...config.resolve,
+//       modules: [path.resolve(__dirname, "../src"), ...config.resolve.modules]
+//     }
+//   };
+// };
+
 const path = require("path");
 
 module.exports = async ({ config, mode }) => {
@@ -6,6 +18,19 @@ module.exports = async ({ config, mode }) => {
     resolve: {
       ...config.resolve,
       modules: [path.resolve(__dirname, "../src"), ...config.resolve.modules]
+    },
+    module: {
+      ...config.module,
+      rules: [
+        ...config.module.rules,
+        {
+          test: /\.scss$/,
+          use: [
+            'resolve-url-loader',
+            'sass-loader'
+          ]
+        }
+      ]
     }
   };
 };
