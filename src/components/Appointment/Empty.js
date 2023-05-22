d34e98d190c91fd739ecec9a8a503721948e52c3
djepi29 <j3anpi3rr3g3org3s@email.com>
2023-05-22 10:04:41 -0400

    First Appointment views

---
+++ src/components/Appointment/Empty.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Empty(props) {
+  const { onAdd } = props
+
+  const handleClick = () => {
+    onAdd();
+  };
+
+  return (
+    <main className="appointment__add">
+  <img
+    className="appointment__add-button"
+    src="images/add.png"
+    alt="Add"
+    onClick={handleClick}
+  />
+</main>
+  );
+}
