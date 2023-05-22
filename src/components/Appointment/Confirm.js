3f6ada44f12f9bc17afdf91097aad9bdf1128cd7
djepi29 <j3anpi3rr3g3org3s@email.com>
2023-05-22 11:55:25 -0400

    Confirm & Show component

---
+++ src/components/Appointment/Confirm.js
@@ -0,0 +1,15 @@
+import Button from 'components/Button';
+import React from 'react'
+
+export default function Show(props) {
+  const { message, onConfirm, onCancel } = props
+  return (
+    <main className="appointment__card appointment__card--confirm">
+  <h1 className="text--semi-bold">{message}</h1>
+  <section className="appointment__actions">
+    <Button onClick={onCancel} danger>Cancel</Button>
+    <Button onClick={onConfirm} danger>Confirm</Button>
+  </section>
+</main>
+  );
+} 
--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,9 @@ import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm"
+
 
 
 
@@ -151,3 +154,18 @@ storiesOf("InterviewerList", module)
   .add("Appointment with time", () => <Appointment time="12pm"/> )
   .add("Header", () => <Header time="12pm"/>)
   .add("Empty", () => <Empty onAdd={action("onAdd")}/>)
+  .add("Show", () => (
+  <Show 
+  student="Lydia Miller-Jones"
+  interviewer="Sylvia Palmer"
+  onEdit={action("onEdit")}
+  onDelete={action("onDelete")}
+   />
+   ))
+   .add("Confirm", () => (
+    <Confirm 
+    message="Delete the appointment?"
+    onConfirm={action("onConfirm")}
+    onCancel={action("onCancel")}
+    />
+   ))
