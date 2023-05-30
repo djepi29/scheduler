8e330372966d943e69516c76ec925f9f1268767c
djepi29 <j3anpi3rr3g3org3s@email.com>
2023-05-30 11:27:06 -0400

    useVisualMode hook

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+
+export default function useVisualMode(initial) {
+
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial])
+
+  const transition = (newMode, replace = false) => {
+    setMode(newMode);
+    setHistory((prevHistory) => {
+      if (replace) {
+        return [...prevHistory.slice(0, -1), newMode];
+      }
+      return [...prevHistory, newMode];
+    });
+  };
+
+  const back = () => {
+    if (history.length > 1) {
+      const newHistory = [...history];
+      newHistory.pop();
+      setMode(newHistory[newHistory.length - 1]);
+      setHistory(newHistory);
+    }
+  };
+
+  return { mode, transition, back };
+}
