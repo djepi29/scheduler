3321ba6623aa5a71c6269a6290acf42261fe5faf
djepi29 <j3anpi3rr3g3org3s@email.com>
2023-05-30 23:39:28 -0400

    using useVisualMode

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,18 +3,41 @@ import Empty from './Empty';
 import Header from './Header';
 import Show from './Show';
 import "./styles.scss";
+import useVisualMode from 'hooks/useVisualMode';
+import Form from './Form';
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREAT = "CREAT";
 
 export default function Appointment(props) {
   const { time, interview } = props;
   const interviewer = { id: 1, name: "John Doe", avatar: "avatar.jpg" }; // Replace with your actual interviewer object
 
+  const { mode, transition, back } = useVisualMode(interview ? SHOW : EMPTY);
+
+  const handleAdd = () => {
+    transition(CREAT);
+  }
+
+  const handleCancel = () => {
+    back();
+  }
+
+
   return (
     <article className="appointment">
       <Header time={time} />
-      {interview ? (
-        <Show student={interview.student} interviewer={interviewer.name} /> // Pass the interviewer name as a prop
-      ) : (
-        <Empty />
+      {mode === SHOW && (
+        <Show student={interview.student} interviewer={interviewer.name} /> 
+      )}
+      {mode === EMPTY && <Empty  onAdd={handleAdd}/>}
+      {mode === CREAT && (
+        <Form   
+        interviewers={[]}
+        onCancel= {handleCancel}
+        
+        />
       )}
     </article>
   );
